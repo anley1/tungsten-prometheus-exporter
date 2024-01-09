6d2ac0eddc9e71c8f9dc1ef9f0f7287c056b715c
Alexander Leye <alexander.leye@team.telstra.com>
2024-01-09 14:40:46 +1100

    register before load

--- tungsten_prometheus_exporter/main.py
+++ tungsten_prometheus_exporter/main.py
@@ -46,11 +46,9 @@ def main():
     args = parser.parse_args()
 
     auth = None
-    keystone_group = cfg.OptGroup('keystone_auth')
+    grp = cfg.OptGroup('keystone_auth')
 
-
-    conf = cfg.ConfigOpts()
-    keystone_opts = [
+    opts = [
         cfg.StrOpt('auth_type', default='v3password', help='Authentication type'),
         cfg.StrOpt('username', help='Username'),
         cfg.StrOpt('password', help='Password'),
@@ -58,18 +56,20 @@ def main():
         cfg.StrOpt('project_name', help='Project name'),
         cfg.StrOpt('project_domain_name', default='Default', help='Project domain name'),
         cfg.StrOpt('user_domain_name', default='Default', help='User domain name'),
+        # cfg.BoolOpt('auth_section', default=False)
     ]
-    conf.register_group(keystone_group)
-    conf.register_opts(keystone_opts, group=keystone_group)
 
-    if args.auth_config:
-        conf(["--config-file", args.auth_config])
+    cfg.CONF.register_group(grp)
+    cfg.CONF(["--config-file", args.auth_config])
+    loading.register_auth_conf_options(cfg.CONF, group='keystone_auth')
+
     if args.config:
         Config().set_file(args.config)
     if args.host:
         Config().set({'analytics': {'host': args.host}})
     Config().render()
-    auth = loading.load_auth_from_conf_options(conf, group="keystone_auth")
+
+    auth = loading.load_auth_from_conf_options(cfg.CONF, group='keystone_auth')
 
     start_http_server(port=Config().prometheus.port)
     logging_format = '%(asctime)-15s:%(levelname)s:%(module)s:%(message)s'
